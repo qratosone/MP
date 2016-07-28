6b5ea32c54eedc88ce2db6b3a12a0a0ca4159529
qratosone <qratosone@live.com>
2016-07-28 15:41:07 +0800

    freopen

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 all: your_vector.out
 	./your_vector.out
 your_vector.out:your_vector.cpp
-	g++ -o your_vector.out your_vector.cpp
+	g++ -o your_vector.out your_vector_solutions.cpp
 clean:
 	rm your_vector.out
--- your_vector.cpp
+++ your_vector.cpp
@@ -67,6 +67,7 @@ public:
 
 
 int main(){
+    freopen("testcase.in","r",stdin);
     vector v1(5);
     for (int i = 0; i <7 ; ++i) {
         v1.insert(i);
@@ -74,8 +75,10 @@ int main(){
     v1.show();
     vector v2(v1);
     int input;
-    cin>>input;
-    v2.insert(input);
+    for (int i=0;i<5;i++){
+        cin>>input;
+        v2.insert(input);
+    }
     v2.show();
     v1.show();
     v2.sort_array();

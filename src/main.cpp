3eb929e2a00931b5695ce94b905c709dd61fd0ca
The space of Qratosone <qratosone@live.com>
2016-08-02 16:26:01 +0800

    update

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include "your_vector.h"
+int main(){
+    freopen("testcase.in","r",stdin);
+    vector v1(5);
+    for (int i = 0; i <7 ; ++i) {
+        v1.insert(i);
+    }
+    v1.show();
+    vector v2(v1);
+    int input;
+    for (int i=0;i<5;i++){
+        cin>>input;
+        v2.insert(input);
+    }
+    v2.show();
+    v1.show();
+    v2.sort_array();
+    v2.show();
+    return 0;
+}
